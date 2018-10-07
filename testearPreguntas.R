d67208d51da9f138b0b6a46d9641b00b97124107
mentoldo <matias.alfonso@gmail.com>
2018-10-07 20:29:55 -0300

    Se agregaron nuevas preguntas hasta R2018_0030.

--- testearPreguntas.R
+++ testearPreguntas.R
@@ -1,6 +1,24 @@
 library(exams)
 
-exams2html(rep("./preguntas/html/R2018_0029.Rmd", 50),
+exams2html(rep("./preguntas/html/R2018_0030.Rmd", 50),
            dir = "render",
            n = 3,
            name = "rand")
+
+
+## Exportamos muchas preguntas
+
+## Creamos una función para exportar las preguntas
+preg <- list.files("./preguntas/html", full.names = TRUE)
+
+# preg <- preg[20 : length(preg)]
+
+exportar <- function(x) {
+    exams2html(rep(x, 100),
+                 n = 1, # La cantidad de preguntas
+                 name = gsub("./preguntas/html/|.Rmd", "", x), # Construye el nombre a partir del nombre original
+                 dir = "render") # Manda la salida a la carpeta moodle
+}
+
+## Exportamos todas las preguntas a un html
+lapply(preg, exportar)
