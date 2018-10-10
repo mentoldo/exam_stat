9cab846851a2289997a0775fd33b885ee46f29a2
mentoldo <matias.alfonso@gmail.com>
2018-10-09 22:10:03 -0300

    Se agrego pregunta con tabla de contingencia

--- testearPreguntas.R
+++ testearPreguntas.R
@@ -1,6 +1,6 @@
 library(exams)
 
-exams2html(rep("./preguntas/html/R2018_0039.Rmd", 50),
+exams2html(rep("./preguntas/html/R2018_0040.Rmd", 50),
            dir = "render",
            n = 3,
            name = "rand")
